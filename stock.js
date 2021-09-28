7235a5170a267fdd9a1c0cf9bc4ed0ffb5f9c943
lsieminski <lukasz.sieminski1@gmail.com>
2021-09-28 14:47:09 +0200

    Modify choice menu + add club filtering function

--- stock.js
+++ stock.js
@@ -14,3 +14,15 @@ function expander(nr)
     subcategories.style.display = "none";
   }
 }  
+
+function club_choice(nr)
+{
+  var choosen_club = document.getElementById('club-'+(nr)).innerHTML;
+  var product_club_name = document.getElementsByClassName("product-club-name");
+
+  for (i=0; i<product_club_name.length; i++)
+  {
+    if (product_club_name[i].innerHTML.trim() == choosen_club.trim()) product_club_name[i].parentElement.style.display = "block";
+    else product_club_name[i].parentElement.style.display = "none";
+  }
+}
