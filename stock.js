cd8f23f4a95808e165b6f8444a93977e46857cb2
lsieminski <lukasz.sieminski1@gmail.com>
2021-10-11 08:21:54 +0200

    Add filtering to category title

--- stock.js
+++ stock.js
@@ -32,12 +32,10 @@ function club_choice(nr)
         if (product_club_name[i].innerHTML.trim() == choosen_club.trim()) 
         {
           club_filter[i] = true;
-          console.log("Filtr klubów", i, club_filter);
         }
         else 
         {
           club_filter[i] = false;
-          console.log("Filtr klubów", i, club_filter);
         }
     }
 
@@ -80,41 +78,85 @@ function category_choice(nr)
   var choosen_category = document.getElementById('category-'+(nr));
   var category_name = document.getElementsByClassName("product");
  
+ 
   category_filter_indicator = true;
 
-  for (i=0; i<category_name.length; i++)
-    {
-        if (category_name[i].className.substring(8) == choosen_category.className.substring(14)) 
-        {
-          category_filter[i] = true;
-          console.log("Filtr klubów", i, category_filter);
-        }
-        else 
-        {
-          category_filter[i] = false;
-          console.log("Filtr klubów", i, category_filter);
-        }
-    }
+  if (choosen_category.className == "category-title")
+{
+  var sibling = choosen_category.nextElementSibling;
+  var subcategory = sibling.children;
 
-    if (club_filter_indicator == true)
+    for (j=0; j<category_name.length; j++)
     {
-      for (i=0; i<category_name.length; i++)
+      for (i=0; i<subcategory.length; i++)
       {
-        if (club_filter[i] == true && category_filter[i] == true)
-        {
-          category_name[i].style.display = "block";
+          if (category_name[j].className.substring(8) == subcategory[i].className.substring(14)) 
+          {
+            category_filter[j] = true;
+            break;
+          }
+          else 
+          {
+            category_filter[j] = false;
+          }
         }
-        else category_name[i].style.display = "none";
       }
-    }
-    else
-    {
-      for (i=0; i<category_name.length; i++)
+
+      if (club_filter_indicator == true)
       {
-        if (category_filter[i] == true) category_name[i].style.display = "block";
-        else category_name[i].style.display = "none";
+        for (j=0; j<category_name.length; j++)
+        {
+          if (club_filter[j] == true && category_filter[j] == true)
+          {
+            category_name[j].style.display = "block";
+          }
+          else category_name[j].style.display = "none";
+        }
       }
+      else
+      {
+        for (j=0; j<category_name.length; j++)
+        {
+          if (category_filter[j] == true) category_name[j].style.display = "block";
+          else category_name[j].style.display = "none";
+        }
+      }
+  }
+  else
+  {
+  for (i=0; i<category_name.length; i++)
+  {
+      if (category_name[i].className.substring(8) == choosen_category.className.substring(14)) 
+      {
+        category_filter[i] = true;
+      }
+      else 
+      {
+        category_filter[i] = false;
+      }
+  }
+
+  if (club_filter_indicator == true)
+  {
+    for (i=0; i<category_name.length; i++)
+    {
+      if (club_filter[i] == true && category_filter[i] == true)
+      {
+        category_name[i].style.display = "block";
+      }
+      else category_name[i].style.display = "none";
     }
+  }
+  else
+  {
+    for (i=0; i<category_name.length; i++)
+    {
+      if (category_filter[i] == true) category_name[i].style.display = "block";
+      else category_name[i].style.display = "none";
+    }
+  }
+}
+
 
   for(i=0; i<document.getElementsByClassName("category-filter-clearing").length; i++)
   {
@@ -126,10 +168,17 @@ function category_choice(nr)
   document.getElementsByClassName("category-item")[i].style.color = "#001e3c";
   }
 
+  for(i=0; i<document.getElementsByClassName("category-title").length; i++)
+  {
+  document.getElementsByClassName("category-title")[i].style.color = "#001e3c";
+  }
+
+
   document.getElementById('category-'+(nr)).previousElementSibling.style.display = "block";
   document.getElementById('category-'+(nr)).style.color = "#ff3333";
 
 }
+
 
 function clear_club_filters()
 {
